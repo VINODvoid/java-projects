b227a216e2fb262be2d072f52b5a7705fd7e6171
VINODvoid <iamlearner.v@gmail.com>
2025-04-21 15:09:06 +0530

    feat: add initial implementation of banking system with account management

--- Bank.java
+++ Bank.java
@@ -1,58 +1,86 @@
+import java.util.Scanner;
+
 abstract class Account {
     private int accountNumber;
-    protected Double balance ;
-    Account(int accountNumber,Double balance)
-    {
-        this.accountNumber  = accountNumber;
+    protected Double balance;
+
+    Account(int accountNumber, Double balance) {
+        this.accountNumber = accountNumber;
         this.balance = balance;
     }
-    public int getAccountNumber()
-    {
+
+    public int getAccountNumber() {
         return accountNumber;
     }
-    public double getBalance()
-    {
+
+    public double getBalance() {
         return balance;
-   }
-    public Double withdraw(Double amount)
-    {
-        return balance -=amount;
     }
+
+    public Double withdraw(Double amount) {
+        return balance -= amount;
+    }
+
     abstract double calculateInterest();
 }
-class SavingsAccount extends Account{
+
+class SavingsAccount extends Account {
     double interestRate;
-    SavingsAccount(int accountNumber,double balance ,double interestRate)
-    {
-        super(accountNumber,balance);
+
+    SavingsAccount(int accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
         this.interestRate = interestRate;
     }
+
     @Override
-    public double calculateInterest()
-    {
-        return balance*interestRate;
+    public double calculateInterest() {
+        return balance * interestRate;
     }
 }
-class CheckingAccount extends Account
-{
+
+class CheckingAccount extends Account {
     double monthlyFee;
-    CheckingAccount(int accountNumber,double balance , double monthlyFee)
-    {
+
+    CheckingAccount(int accountNumber, double balance, double monthlyFee) {
         super(accountNumber, balance);
         this.monthlyFee = monthlyFee;
     }
 
     @Override
-    double calculateInterest() { 
+    double calculateInterest() {
         return 0.0;
     }
 }
 
 public class Bank {
     public static void main(String[] args) {
-        SavingsAccount savingAccount = new SavingsAccount(1234567, 12.89, 3.0);
-        CheckingAccount checkingAccount = new CheckingAccount(2002, 1000.0, 12.5); 
-        System.out.println(savingAccount.calculateInterest());
-        System.out.println(checkingAccount.calculateInterest());
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Savings Account details:");
+        System.out.print("Account Number: ");
+        int savingsAccountNumber = scanner.nextInt();
+
+
+        System.out.print("Balance: ");
+        double savingsBalance = scanner.nextDouble();
+
+        System.out.print("Interest Rate: ");
+        double interestRate = scanner.nextDouble();
+        SavingsAccount savingAccount = new SavingsAccount(savingsAccountNumber, savingsBalance, interestRate);
+
+        System.out.println("Enter Checking Account details:");
+        System.out.print("Account Number: ");
+        int checkingAccountNumber = scanner.nextInt();
+        System.out.print("Balance: ");
+        double checkingBalance = scanner.nextDouble();
+        System.out.print("Monthly Fee: ");
+        double monthlyFee = scanner.nextDouble();
+        CheckingAccount checkingAccount = new CheckingAccount(checkingAccountNumber, checkingBalance, monthlyFee);
+
+        
+        System.out.println("Savings Account Interest: " + savingAccount.calculateInterest());
+        System.out.println("Checking Account Interest: " + checkingAccount.calculateInterest());
+
+        scanner.close();
     }
 }
