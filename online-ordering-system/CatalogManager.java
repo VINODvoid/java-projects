80a34ca601c3201226a775e2e0ad3f057f787b99
VINODvoid <iamlearner.v@gmail.com>
2025-05-06 17:48:37 +0530

    feat : add catalogManager

---
+++ online-ordering-system/CatalogManager.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CatalogManager {
+    private Map<Integer,Product> catalog = new HashMap<>();
+
+    public void addProduct(Product product )
+    {
+        catalog.put(product.ID, product);
+    }
+    public void displayProducts()
+    {
+        System.out.println("Products Catalog");
+        for(Product product : catalog.values())
+        {
+            System.out.println(product);
+        }
+    }
+    public Product getProductbyId(int id)
+    {
+        return catalog.get(id);
+    }
+
+    public boolean hasProduct(int id)
+    {
+        return catalog.containsKey(id);
+    }
+}
